7c96887d2f5fd716205b03d8d6bbaf4ba9f0a28c
manisha-20048 <gosulamanisha5@gmail.com>
2026-04-30 22:35:44 +0530

    added ssl for aiven

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ mydb=mysql.connector.connect( host=os.getenv("DB_HOST"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME"),
-    port=int(os.getenv("DB_PORT")))
+    port=int(os.getenv("DB_PORT")),
+    ssl_disabled=False
 cursor=mydb.cursor(buffered=True)
 
 
